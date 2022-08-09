3022769da8488a94105ff1bc9bcc2c3efc45e7b8
blanc252023 <h.baolan20022@gmail.com>
2022-08-09 19:54:20 +0700

    Authorization WorkFlow

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const connectDB = require("./db/connect");
 
 // routers
 const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
 
 //  middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -24,11 +25,8 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
-app.get("/api/v1", (req, res) => {
-  console.log(req.signedCookies);
-  res.send("E-commerece");
-});
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
 
 // custom middleware
 app.use(notFoundMiddleware);
---
+++ routes/userRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const {
+  authenticateUser,
+  authorizePermisions,
+} = require("../middleware/authentication");
+const {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+} = require("../controllers/userController");
+
+router
+  .route("/")
+  .get(authenticateUser, authorizePermisions("admin", "user"), getAllUsers);
+
+router.route("/showMe").get(showCurrentUser);
+
+router.route("/updateUser").patch(updateUser);
+
+router.route("/updateUserPassword").patch(updateUserPassword);
+
+router.route("/:id").get(authenticateUser, getSingleUser);
+
+module.exports = router;
