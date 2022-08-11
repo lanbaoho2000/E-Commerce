a24cc275cc63bb52b62f149b649005d9be638f9b
blanc252023 <h.baolan20022@gmail.com>
2022-08-11 14:00:08 +0700

    Working with Model Virtuals

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -26,7 +26,11 @@ const createReview = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ review });
 };
 const getAllReviews = async (req, res) => {
-  const reviews = await Review.find({});
+  const reviews = await Review.find({}).populate({
+    path: "product",
+    select: "name company price",
+  });
+
   res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
 };
 const getSingleReview = async (req, res) => {
