3c3cfce32d8218c5e87948b574ee5906ffd71604
blanc252023 <h.baolan20022@gmail.com>
2022-08-11 13:45:06 +0700

    Delete and Update Review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -39,10 +39,32 @@ const getSingleReview = async (req, res) => {
   res.status(StatusCodes.OK).json({ review });
 };
 const updateReview = async (req, res) => {
-  res.send("update review");
+  const { id: reviewId } = req.params;
+  const { rating, title, comment } = req.body;
+  const review = await Review.findOne({ _id: reviewId });
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+  }
+
+  checkPermision(req.user, review.user);
+  review.rating = rating;
+  review.title = title;
+  review.comment = comment;
+
+  await review.save();
+  res.status(StatusCodes.OK).json({ msg: "Success! Review removed" });
 };
 const deleteReview = async (req, res) => {
-  res.send("delete review");
+  const { id: reviewId } = req.params;
+  const review = await Review.findOne({ _id: reviewId });
+
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+  }
+
+  checkPermision(req.user, review.user);
+  await review.remove();
+  res.status(StatusCodes.OK).json({ msg: "Success! Review removed" });
 };
 
 module.exports = {
